c21a9330fee54e98e30644f4394d9a2668a3147a
Anežka Vítová <283200@vutbr.cz>
2026-04-13 13:20:36 +0200

    Commit message

--- searching.py
+++ searching.py
@@ -20,18 +20,35 @@ def read_data(file_to_read, klicek):
     # get current working directory path
     cwd_path = Path.cwd()
     
-    file_path = cwd_path / file_name
-    with open(file_to_read, "r", encoding="utf-8") as filik:
+    file_path = cwd_path / file_to_read
+    with open(file_path, "r", encoding="utf-8") as filik:
         soubor = json.load(filik)
 
         if klicek not in soubor:
             return None
         return soubor[klicek]
 
+def linear_search(cast, cislicko):
+    indexiky=[]
+    kolikrat = 0
+    for i in cast:
+        if i==cislicko:
+            kolikrat+=1
+
+    for indexik, y in enumerate(cast):
+        if y == cislicko:
+            indexiky.append(indexik)
+    slovnicek={
+        "position": indexiky,
+        "count": kolikrat
+    }
+    return slovnicek
+
 def main():
     sequential_data = read_data('sequential.json', 'unordered_numbers')
     print(sequential_data)
-
+    kolik=linear_search(sequential_data, 5)
+    print(kolik)
 
 if __name__ == "__main__":
     main()
